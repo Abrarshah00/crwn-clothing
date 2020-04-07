a447c3705ffdb406cf4fececc99cfef9d8bda864
Abrarshah00 <abrarshah00@gmail.com>
2020-04-07 19:45:02 +0530

    updated shop page and added styles to it

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,32 +12,37 @@ class Directory extends React.Component {
       sections: [
         {
           title: "hats",
-          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+          imageUrl:
+            "https://images.pexels.com/photos/954254/pexels-photo-954254.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
           id: 1,
           linkUrl: "shop/hats",
         },
         {
           title: "jackets",
-          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+          imageUrl:
+            "https://i2.wp.com/www.everydaymrs.com/wp-content/uploads/2017/07/IMG_0629.jpg?ssl=1",
           id: 2,
           linkUrl: "shop/jackets",
         },
         {
           title: "sneakers",
-          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+          imageUrl:
+            "https://images.pexels.com/photos/1619652/pexels-photo-1619652.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
           id: 3,
           linkUrl: "shop/sneakers",
         },
         {
           title: "womens",
-          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+          imageUrl:
+            "https://images.pexels.com/photos/1536619/pexels-photo-1536619.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
           size: "large",
           id: 4,
           linkUrl: "shop/womens",
         },
         {
           title: "mens",
-          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+          imageUrl:
+            "https://images.pexels.com/photos/842811/pexels-photo-842811.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
           size: "large",
           id: 5,
           linkUrl: "shop/mens",
